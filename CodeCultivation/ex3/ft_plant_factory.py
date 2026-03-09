d064dfd8595200812e611144b909b91013fc31a9
Ahmad Kamamji <akamamji@c2r4s8.irbid.42.school>
2026-03-09 10:44:02 +0300

    Solve the forth exercise of CodeCultivation

---
+++ CodeCultivation/ex3/ft_plant_factory.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+class Plant:
+    count: int = 0
+    def __init__(self, name: str = "Plant", height: int = 0, age: int = 0) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+        print(f"Created: {self.name} ({self.height}cm, {self.age} days)")
+        Plant.count += 1
+
+
+def main() -> None:
+    print("=== Plant Factory Output ===")
+    plant_one = Plant("Rose",25,30)
+    plant_two = Plant("Oak",200,365)
+    plant_three = Plant("Cactus",5,90)
+    plant_four = Plant("Sunflower",80,45)
+    plant_five = Plant("Fern",15,120)
+    print(f"\nTotal plants created: {Plant.count}")
+
+
+if __name__ == "__main__":
+    main()
+
