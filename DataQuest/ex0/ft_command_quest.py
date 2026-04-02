fd5a347724fbc9afb1cacdc14401f394ce90a1b5
Ahmad Naji Mahmoud Kamamji <ahmadnajikam@gmail.com>
2026-04-02 07:09:16 +0300

    Fix how the program name is printed

--- DataQuest/ex0/ft_command_quest.py
+++ DataQuest/ex0/ft_command_quest.py
@@ -4,15 +4,14 @@ import sys
 
 def main() -> None:
     print("=== Command Quest ===")
-    i: int = 0
-    print(f"Program name:{sys.argv[0]}")
+    i: int = 1
+    print(f"Program name: {sys.argv[0].replace("./", "")}")
     if len(sys.argv) == 1:
         print("No arguments provided!")
     else:
         print(f"Arguments received:{len(sys.argv) - 1}")
-        for arg in sys.argv:
-            if i != 0:
-                print(f"Argument {i}: {arg}")
+        for arg in sys.argv[1:]:
+            print(f"Argument {i}: {arg}")
             i += 1
     print(f"Total arguments: {len(sys.argv)}")
 
